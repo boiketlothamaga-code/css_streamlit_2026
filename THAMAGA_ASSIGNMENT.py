6fd1ebce8500d0185dc224fa15912e6f098a4256
boiketlothamaga-code <boiketlothamaga@gmail.com>
2026-01-31 15:36:24 +0200

    Add files via upload

--- THAMAGA_ASSIGNMENT.py
+++ THAMAGA_ASSIGNMENT.py
@@ -19,12 +19,13 @@ st.set_page_config(
 # -------------------------------
 # Header section
 # -------------------------------
-st.title("🧪 Gas Sensors & MXene Research Profile")
+st.title("🧪  Thamaga's Portable Gas Sensor Devices")
 
 st.markdown("""
 **Research Focus:**  
-MXene-based gas sensors, metal oxide heterostructures, defect engineering, and surface/interface physics.
-""")
+
+Gas sensors are a category of electronic devices that detect and measure the presence of various gases.
+Gas sensor device detects and monitors hazardous gases—including toxic, flammable, and combustible""")
 
 st.divider()
 
@@ -35,19 +36,25 @@ col1, col2 = st.columns([1, 2])
 
 with col1:
     st.image(
-        "https://upload.wikimedia.org/wikipedia/commons/3/3a/Graphene_structure.svg",
-        caption="2D Materials Concept",
+        "https://www.istockphoto.com/illustrations/gas-detector",
+        caption="Gas sensor device",
         use_container_width=True
     )
 
 with col2:
-    st.subheader("👩🏽‍🔬 About Me")
+    st.subheader("Wearable Gas Sensors")
     st.write("""
-I am a researcher working in the field of **gas sensing and low-dimensional materials**, 
-with a strong emphasis on **MXenes (Ti₃C₂Tₓ)** and **metal oxide–MXene heterostructures** 
-for room-temperature gas sensing.
-
-My research explores **charge transport mechanisms**, **Schottky barriers**, 
-**defect engineering**, and **surface chemistry** to enhance sensitivity, selectivity, 
-and stability of gas sensors.
+Get yourself a wearable gas sensor device for personal safety and health monitoring
     """)
+
+st.write ("The wearable devices come as a patches, badges, and smart accessories, offer real-time, on-person monitoring of hazardous gases (e.g., \(CO\), \(H_{2}S\), \(NH_{3}\)) and volatile organic compounds (VOCs)")
+st.image("https://www.researchgate.net/figure/Types-of-wearable-gas-sensors-Wearable-gas-sensors-Adapted-with-permission56_fig11_349088989")
+
+
+st.write("Protect yourseld and loved ones.")
+st.write("Get a sensing device NOW!")
+
+
+
+
+
