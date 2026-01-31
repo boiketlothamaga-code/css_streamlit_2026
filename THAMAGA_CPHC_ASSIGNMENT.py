99de64db50e71ffa7cce968223c49fbbba4b4604
boiketlothamaga-code <boiketlothamaga@gmail.com>
2026-01-31 18:13:14 +0200

    Add files via upload

--- THAMAGA_CPHC_ASSIGNMENT.py
+++ THAMAGA_CPHC_ASSIGNMENT.py
@@ -54,7 +54,7 @@ page = st.sidebar.radio(
 # Home Page
 # --------------------------------
 if page == "Home" :
-    st.image("C:/Users/THAMAGA/Downloads/THAMAGA_IMAGE.png")
+    st.image("https://chatgpt.com/s/m_697e20a04dc881918074452e43290e78")
     st.subheader("Gas sensors devices to protect you from harmful and toxic gases!")
 # --------------------------------
 # About
