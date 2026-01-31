657f469a377d53f4e46b4062690ff1dec10f13e9
boiketlothamaga-code <boiketlothamaga@gmail.com>
2026-01-31 18:00:10 +0200

    Add files via upload

--- THAMAGA_CPHC_ASSIGNMENT.py
+++ THAMAGA_CPHC_ASSIGNMENT.py
@@ -183,7 +183,7 @@ elif page == "Contact & Orders":
 
     st.markdown("""
 📧 **Email:** boiketlothamaga@ThamagaLab.ac.za  
-📞 **Phone:** +27 67 793 9766  
+📞 **Phone:** +27 13 777 6666 
 🌍 **Location:** Thamaga Research & Innovation Lab  
     """)
 
